4ca2b41ed6fdd1917def7b2a9302421386152f98
James Petersen <jpetersenames@gmail.com>
2021-07-01 17:33:13 -0600

    change todo to kustomize
    Signed-off-by: James Petersen <jpetersenames@gmail.com>

---
+++ scripts/strap.sh
@@ -0,0 +1,22 @@
+export KUBECONFIG=$(k3d kubeconfig write manage)
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+while true; do
+  echo "Waiting for deployment/argocd-server..."
+  if kubectl wait --for=condition=available --timeout 240s deployment/argocd-server -n argocd; then
+    break;
+  fi;
+  sleep 5;
+done
+
+while true; do
+  echo "Waiting for deployment/argocd-repo-server..."
+  if kubectl wait --for=condition=available --timeout 240s deployment/argocd-repo-server -n argocd; then
+    break;
+  fi;
+  sleep 5;
+done
+
+
+helm template apps/ | kubectl apply -f -
